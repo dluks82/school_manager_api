2fee932a15210c8aab92ccbc31d610b86e7989b0
Diogo Lucas de Oliveira <46111243+Dluks82@users.noreply.github.com>
2023-09-21 17:15:23 -0300

    feat: basic CRUD

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ import json
 app = Flask(__name__)
 
 
+# Carregar dados
 def load_data(category_name: str) -> list:
     try:
         with open(category_name+'.json', 'r', encoding='utf-8') as f:
@@ -16,6 +17,7 @@ def load_data(category_name: str) -> list:
         return []
 
 
+# Gravar Dados
 def save_data(data: dict, category_name: str) -> bool:
     try:
         with open(category_name+'.json', 'w', encoding='utf-8') as f:
@@ -26,15 +28,62 @@ def save_data(data: dict, category_name: str) -> bool:
         return False
 
 
+# Rota root
 @app.route('/')
 def root():
     return 'Hello from School Manager!'
 
 
+# Todos os estudantes
 @app.route('/estudantes', methods=['GET'])
 def get_estudantes():
-    data = load_data('estudantes')
-    return jsonify(data)
+    database = load_data('estudantes')
+    return jsonify(database)
+
+
+# Estudante por codigo
+@app.route('/estudante/<int:codigo>', methods=['GET'])
+def get_estudante(codigo):
+    database = load_data('estudantes')
+    for estudante in database:
+        if estudante.get('codigo') == codigo:
+            return jsonify(estudante)
+    return 'Estudante não localizado!'
+
+
+# Criar novo estudante
+@app.route('/estudantes', methods=['POST'])
+def add_estudante():
+    database = load_data('estudantes')
+    new_estudante = request.get_json()
+    database.append(new_estudante)
+    if save_data(database, 'estudantes'):
+        return jsonify(new_estudante)
+    return 'Erro ao adicionar o estudante!'
+
+
+# Excluir estudante
+@app.route('/estudantes/<int:codigo>', methods=['DELETE'])
+def del_estudante(codigo):
+    database = load_data('estudantes')
+    for index, estudante in enumerate(database):
+        if estudante.get('codigo') == codigo:
+            del database[index]
+            if save_data(database, 'estudantes'):
+                return jsonify(estudante)
+        return 'Erro ao excluir o estudante'
+
+
+# Editar estudante
+@app.route('/estudantes/<int:codigo>', methods=['PUT', 'PATCH'])
+def update_estudante(codigo):
+    updated_estudante = request.get_json()
+    database = load_data('estudantes')
+    for index, estudante in enumerate(database):
+        if estudante.get('codigo') == codigo:
+            database[index].update(updated_estudante)
+            save_data(database, 'estudantes')
+            return jsonify(database[index])
 
 
 # port 500 is already used in macOS

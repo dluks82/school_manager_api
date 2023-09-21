545c62fef784a76abeaf857cb980b52d8b1add0d
Diogo Lucas de Oliveira <46111243+Dluks82@users.noreply.github.com>
2023-09-21 16:29:14 -0300

    feat: load and save data code

--- src/app.py
+++ src/app.py
@@ -1,6 +1,29 @@
 from flask import Flask, jsonify, request
+import json
 
 app = Flask(__name__)
+
+
+def load_data(category_name: str) -> list:
+    try:
+        with open(category_name+'.json', 'r', encoding='utf-8') as f:
+            return json.load(f)
+    except FileNotFoundError as exception:
+        print(f'Error on loading data: {exception}')
+        return []
+    except Exception as exception:
+        print(f'Error on loading data: {exception}')
+        return []
+
+
+def save_data(data: dict, category_name: str) -> bool:
+    try:
+        with open(category_name+'.json', 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False)
+            return True
+    except Exception as exception:
+        print(f'Error on saving data: {exception}')
+        return False
 
 
 @app.route('/')
